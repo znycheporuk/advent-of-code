2880e43a35a93d8837855f9ef55fde7273f0c48d
znycheporuk <nycheporuk.zakhar@gmail.com>
2022-12-08 01:26:53 +0200

    day 4 completed

---
+++ 2022/4/task2.ts
@@ -0,0 +1,26 @@
+import { readFileSync } from 'fs';
+
+
+const input = readFileSync('./input.txt').toString();
+
+const pairs = input.split('\n');
+
+function calculateOverlap([xMin, xMax, yMin, yMax]: number[]) {
+	// fully contains
+	if (xMin >= yMin && xMax <= yMax) return [xMin, xMax];
+	if (yMin >= xMin && yMax <= xMax) return [yMin, yMax];
+
+	// partially overlaps
+	if (xMin >= yMin && xMin <= yMax) return [xMin, yMax];
+	if (yMin >= xMin && yMin <= xMax) return [yMin, xMax];
+
+	return [];
+}
+
+const overlaps = pairs.map(p => p.split(/[-,]+/).map(Number)).map(calculateOverlap);
+
+const filteredPairs = overlaps.filter(item => item.length);
+
+const result = filteredPairs.length;
+
+console.log(result);
